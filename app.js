96be7e2cb63cb54ccc16d208b330200c6e534046
aderemi azeez <aderemiazeez6@gmail.com>
2025-02-25 09:37:37 -0800

    Updated deployment settings for vercel

--- app.js
+++ app.js
@@ -86,11 +86,10 @@ app.post("/contact", function(req, res, next){
 
 
 module.exports = app;
-/*
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
 console.log("Express started. Listening on port %s", port);
 });
 
 // Removed JSON content
-*/
